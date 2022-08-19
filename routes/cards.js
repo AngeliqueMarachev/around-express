78163a6e527e92f5bbea2d7531bc94b3749523f8
AngeliqueMarachev <angeliquealive@gmail.com>
2022-08-19 16:35:18 +0300

    Build API, create file structure, and use fs module to read .json data

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,17 @@
 const router = require('express').Router();
 const fs = require('fs').promises;
+const path = require('path');
 
-router.get('/', (req, res) => {
-  fs.readFile('../data/cards.json', { encoding: 'utf-8' })
+const DATA_PATH = path.join(__dirname, '../data/cards.json');
+
+router.get('/cards', (req, res) => {
+  fs.readFile(DATA_PATH, { encoding: 'utf-8' })
     .then((cards) => {
       res.send(cards)
   })
-  .catch(() => res.status(500).send({ message: "We have encountered an error" }));
+    .catch(() =>
+      res.status(500).send({ message: "We have encountered an error" })
+    );
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,22 @@
-const router = require('express').Router();
-const fs = require('fs').promises;
+const router = require("express").Router();
+const fs = require("fs").promises;
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
+const path = require('path');
 
-  fs.readFile('../data/users.json', { encoding: "utf-8" }) // read users fs promise
-    .then((users) => {
-      const data = JSON.parse(users);
-      const user = data.find((user) => user._id === id); // find user by id
+const DATA_PATH = path.join(__dirname, '../data/users.json');
+
+router.get("/users", (req, res) => {
+  fs.readFile(DATA_PATH, { encoding: "utf-8" })
+    .then((data) =>
+    res.send(data)
+  );
+});
+
+router.get("/users/:id", (req, res) => {
+  fs.readFile(DATA_PATH, { encoding: "utf-8" })
+    .then((data) => {
+      const { id } = req.params;
+      const user = JSON.parse(data).find((user) => user._id === id); // find user by id
 
       if (user) {
         res.send(user);
@@ -15,18 +24,11 @@ router.get('/:id', (req, res) => {
         res.status(404).send({ message: "User ID not found" }); // user does not exist
       }
     })
-    .catch(() =>
-      res.status(500).send({ message: "We have encountered an error" })
-    ); // respond server error
+    .catch((err) => {
+     console.log('err =>', err)
+      res.status(500).send({ message: "We have encountered an error" }) // respond server error
+    });
 });
-
-router.get("/", (req, res) => {
-  res.status(200).send({ data: [] });
-});
-
-// router.get("/:id", (req, res) => {
-//   req.params.id;
-//   res.send({ message: "User ID not found" });
-// });
 
 module.exports = router;
+
